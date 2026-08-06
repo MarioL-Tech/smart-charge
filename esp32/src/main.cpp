e3d5b0493e20357947f9aaa91b898ee841c3bdc9
Mario L <mario.leeb0303@gmx.at>
2026-08-06 16:56:23 +0200

    add

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -4,10 +4,12 @@
 void setup() {
   // Serial2 uses GPIO 16 (RX) and 17 (TX) on most ESP32s
   Serial2.begin(115200, SERIAL_8N1, 16, 17); 
+  Serial.begin(115200);
 }
 
 void loop() {
   // Send a test message to the Pi every 2 seconds
   Serial2.println("EV_STATUS:READY");
+  Serial.println("EV_STATUS:READY");
   delay(2000);
 }
